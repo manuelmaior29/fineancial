be62abf7306e8112b3adac37c2fcf3b9c0124cbe
Manuel-Razvan Maior <manuelmaior.29@gmail.com>
2025-02-26 18:14:25 +0200

    Started the integration of the clasif.

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
+import json
 import streamlit as st
 import pandas as pd
 import io
 from src.preprocessing.parser import *
+from src.models.rulebased import *
 
-STANDARD_FIELDS = [
-    "Transaction type", "Category", "Label", "Date",
-    "Cleaned Transaction Description", "Notes", "Amount", "Currency"
-]
 
 if "transactions" not in st.session_state:
-    st.session_state.transactions = pd.DataFrame(columns=STANDARD_FIELDS)
+    st.session_state.transactions = pd.DataFrame()
+if "transactions_classification_rules" not in st.session_state:
+    st.session_state.transactions_classification_rules = {}
+if "transactions_classifier_model" not in st.session_state:
+    st.session_state.transactions_classifier_model = RuleBasedClassifier()
+
+def parse_classification_rules(file):
+    try:
+        classification_rules = {}
+        with open(file, "r") as f:
+            classification_rules = json.load(f)
+        return classification_rules
+    except Exception as e:
+        st.error(f"Error parsing the classification rules: {e}")
+        return {}
 
 def parse_xlsx(file):
     try:
         # TODO: Replace hardcoded parser
         parser = BTParser()
-        transaction_data = parser.parse(document_path=file)
+        transaction_data = parser.parse(document_path=r"C:\Users\mam2clj\Documents\Personal\Documents\RO35BTRLRONCRT0285357701-01.01.2025-01.02.2025.csv")
         return pd.DataFrame([transaction.__dict__ for transaction in transaction_data])
-
     except Exception as e:
         st.error(f"Error processing file: {e}")
-        return pd.DataFrame(columns=STANDARD_FIELDS)
+        return pd.DataFrame()
 
+st.set_page_config(layout='wide')
 st.title("💰 Bank Transactions Parser")
 uploaded_file = st.file_uploader("Upload a bank statement (CSV)", type=["csv"])
 if uploaded_file is not None:
@@ -34,7 +46,7 @@ if uploaded_file is not None:
         )
 
 st.subheader("📊 All Transactions")
-st.dataframe(st.session_state.transactions)
+st.data_editor(st.session_state.transactions)
 st.subheader("⬇️ Download Transactions")
 buffer = io.BytesIO()
 st.session_state.transactions.to_excel(buffer, index=False)
