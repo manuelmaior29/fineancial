e9f343b9bc6e2ace82b63d1eb31ef38957878b92
manuelmaior.29 <manuelmaior.29@gmail.com>
2025-04-22 21:21:52 +0300

    Started modifications for multiple file upload support

--- ui/state.py
+++ ui/state.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 def init():
     default_values = {
+        "data_transactions": pd.DataFrame(),
+        "input_data_transactions": 0,
         "interval_type": "Weekly",
-        # FIXME: Fix initialization of interval start and end
         "current_interval_start": pd.to_datetime(datetime.now().replace(day=1)),
         "current_interval_end": pd.to_datetime(datetime.now()),
     }
